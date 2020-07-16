754813880c46b8955b7e67f80180604b1f820948
jabershaker <66861006+jabershaker@users.noreply.github.com>
2020-07-17 01:35:03 +0300

    first assignment

--- assignment.js
+++ assignment.js
@@ -1,4 +1,13 @@
-feetToMile
-woodCalculator
-brickCalculator
-tinyFriend
+function feetToMile(feet) {
+    var mile = feet / 5280;
+    return mile;
+
+}
+var mileCalculator = feetToMile(10000);
+console.log(mileCalculator);
+
+var mileCalculator1 = feetToMile(40000);
+console.log(mileCalculator1);
+
+var mileCalculator2 = feetToMile(75000);
+console.log(mileCalculator2);
