87bc931e81c8bbb76634f7e89daeab89492a52fb
jabershaker <66861006+jabershaker@users.noreply.github.com>
2020-07-17 02:47:16 +0300

    2nd assignment

--- assignment.js
+++ assignment.js
@@ -1,7 +1,7 @@
+//feetToMile
 function feetToMile(feet) {
     var mile = feet / 5280;
     return mile;
-
 }
 var mileCalculator = feetToMile(10000);
 console.log(mileCalculator);
@@ -11,3 +11,21 @@ console.log(mileCalculator1);
 
 var mileCalculator2 = feetToMile(75000);
 console.log(mileCalculator2);
+
+//woodCalculator
+function woodCalculator(chair, table, bed) {
+    var chaircount = chair * 1;
+    var tablecount = table * 3;
+    var bedcount = bed * 5;
+    var totalWood = chaircount + tablecount + bedcount;
+    return totalWood;
+}
+var woodResult = woodCalculator(20, 12, 7);
+console.log(woodResult);
+
+//brickCalculator
+function brickCalculator() {
+    
+
+}
+
